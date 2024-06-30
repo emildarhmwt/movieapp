19f8468530449e1e8b0a57c0379a2f4cfc6568b5
emildarhmwt <emildarahmawati31@gmail.com>
2024-06-30 07:52:58 +0700

    memperbaiki search

--- src/navigations/BottomTabNavigation.tsx
+++ src/navigations/BottomTabNavigation.tsx
@@ -6,6 +6,7 @@ import Search from '../screens/Search'
 import Favorite from '../screens/Favorite'
 import HomeStackNavigation from './HomeStackNavigation';
 import FavoriteStackNavigation from './FavoriteStackNavigation'
+import SearchStackNavigation from './SearchStackNavigation'
 
 const Tab = createBottomTabNavigator()
 
@@ -24,7 +25,7 @@ const BottomTabNavigator = (): JSX.Element => (
     />
         <Tab.Screen
         name = "Search"
-        component={Search}
+        component={SearchStackNavigation}
         options={{
             tabBarIcon: ({color}) => (
                 <Feather name="search" size={28} color={color}/>
